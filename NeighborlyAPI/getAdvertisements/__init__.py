767c5b3370f528befea55c478e535fdbef7671c8
Aparna <knaparnashriraksha@gmail.com>
2021-10-29 11:35:21 -0500

    update api and frontend

--- NeighborlyAPI/getAdvertisements/__init__.py
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -6,9 +6,9 @@ from bson.json_util import dumps
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+        url = "mongodb://neighborlyappcosmosdb:dWQvJdA4Xq1d6jk1Pu6BGnKEoKaztZPy8mOQOKlh3NPVxvGIcxCq9NkH1PzXuBIb82FZzwBNH0DQ6Xq3wGiJ9Q==@neighborlyappcosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborlyappcosmosdb@"
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['MongoDB']
         collection = database['advertisements']
 
 
--- NeighborlyAPI/getPosts/__init__.py
+++ NeighborlyAPI/getPosts/__init__.py
@@ -10,9 +10,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python getPosts trigger function processed a request.')
 
     try:
-        url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+        url = "mongodb://neighborlyappcosmosdb:dWQvJdA4Xq1d6jk1Pu6BGnKEoKaztZPy8mOQOKlh3NPVxvGIcxCq9NkH1PzXuBIb82FZzwBNH0DQ6Xq3wGiJ9Q==@neighborlyappcosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborlyappcosmosdb@"
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['MongoDB']
         collection = database['posts']
 
         result = collection.find({})
